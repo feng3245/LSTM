14cf2a84fea9721122fb818f805c9a2e4363ed8b
Feng Liu <li123b@hotmail.com>
2018-09-12 23:23:22 -0400

    Initial commit. Understanding of vanilla LSTM. Updated to tune SGD

---
+++ vanilla/vanilla.py
@@ -0,0 +1,56 @@
+import numpy as np
+from random import randint
+from numpy import array
+from numpy import argmax
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Activation
+from keras import optimizers
+def generate_sequence(length, n_features):
+	return [randint(0, n_features-1) for _ in range(length)]
+
+def one_hot_encode(sequence, n_features):
+	encoding = list()
+	for value in sequence:
+		vector = [0 for _ in range(n_features)]
+		vector[value] = 1
+		encoding.append(vector)
+	return array(encoding)
+def one_hot_decode(encoded_seq):
+	#argmax gives largest index when working on array
+	return [argmax(vector) for vector in encoded_seq]
+def generate_example(length, n_features, out_index):
+	sequence = generate_sequence(length, n_features)
+	encoded = one_hot_encode(sequence, n_features)
+	
+	X = encoded.reshape((1, length, n_features))
+	y = encoded[out_index].reshape(1, n_features)
+	return X, y
+length = 10
+n_features = 15
+out_index = 2
+model = Sequential()
+model.add(LSTM(100, input_shape=[length, n_features]))
+model.add(Dense(n_features, activation ='softmax'))
+sgd = optimizers.SGD(lr=0.007, decay=1e-4, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metric=['accuracy'])
+model.summary()
+
+for i in range(20000):
+	X, y = generate_example(length, n_features, out_index)
+	model.fit(X, y, nb_epoch=1, verbose=2, batch_size=1)
+
+correct = 0
+for i in range(100):
+	X, y = generate_example(length, n_features, out_index)
+	yhat = model.predict(X)
+	if one_hot_decode(yhat) == one_hot_decode(y):
+		correct += 1
+print('Accuracy: %f' % ((correct/100)*100.0))
+
+X, y = generate_example(length, n_features, out_index)
+yhat = model.predict(X)
+print('Sequence: %s' % [one_hot_decode(x) for x in X])
+print('Expected: %s' % one_hot_decode(y))
+print('Predicted: %s' % one_hot_decode(yhat))
